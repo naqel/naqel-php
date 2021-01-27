4cac78870570bd84c0ff474849a97ddaf1734a8a
Mohammad ALTAWEEL <hello@mohammad.tech>
2021-01-27 20:23:29 +0300

    Add Currency list

---
+++ src/Constants/Currency.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Naqel\Constants;
+
+class Currency
+{
+    public const SAR = 1;
+    public const AED = 2;
+    public const USD = 4;
+    public const GBP = 5;
+    public const OMR = 6;
+    public const JOD = 7;
+    public const LBP = 8;
+    public const BHD = 9;
+    public const EGP = 10;
+    public const KWD = 11;
+    public const CNY = 12;
+}
